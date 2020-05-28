00208dd051d2fa0856e1304f2bdedf50cd79c221
Julio Montoya <montoya_julio@me.com>
2020-05-28 15:20:01 -0700

    Added first Networking API for retrieving network data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -10,5 +10,33 @@ import Foundation
 extension RazeCore {
   public class Networking {
     
+    /// Responsible for handling all networking calls
+    /// - Warning: Must creare before using any public API's
+    public class Manager {
+      
+      // MARK: - Properties
+      private let session = URLSession.shared
+      
+      // MARK: - Object Lifecycle
+      public init() {
+        
+      }
+      
+      // MARK: - Cases
+      public enum NetworkResult<Value> {
+        case success(Value)
+        case failure(Error?)
+      }
+      
+      // MARK: - Body
+      public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+        let task = session.dataTask(with: url) { data, response, error in
+          let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+          completionHandler(result)
+        }
+        
+        task.resume()
+      }
+    }
   }
 }
--- Tests/RazeCoreTests/NetworkingTests.swift
+++ Tests/RazeCoreTests/NetworkingTests.swift
@@ -6,14 +6,31 @@
 //
 
 import XCTest
+@testable import RazeCore
 
 class NetworkingTests: XCTestCase {
   
   static var allTests = [
-    ("testExample", testExample)
+    ("testLoadDataCall", testLoadDataCall)
   ]
   
-  func testExample() throws {
+  func testLoadDataCall() {
+    let manager = RazeCore.Networking.Manager()
+    let expectation = XCTestExpectation(description: "Call for data")
+    guard let url = URL(string: "http://raywenderlich.com") else {
+      return XCTFail("Coudl not create URL propertly")
+    }
     
+    manager.loadData(from: url) { result in
+      expectation.fulfill()
+      switch result {
+      case .success(let returnedData):
+        XCTAssertNotNil(returnedData, "Response data is nil")
+      case .failure(let error):
+        XCTFail(error?.localizedDescription ?? "error forming error result")
+      }
+    }
+    
+    wait(for: [expectation], timeout: 5)
   }
 }
