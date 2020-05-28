1e42325f1c21b785f2f44c2a76326911f0cbe61e
Julio Montoya <montoya_julio@me.com>
2020-05-28 16:00:40 -0700

    Refactored first Networking API

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
+protocol NetworkSession {
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+    let task = dataTask(with: url) { data, _, error in
+      completionHandler(data, error)
+    }
+    task.resume()
+  }
+}
+
 extension RazeCore {
   public class Networking {
     
@@ -15,7 +28,7 @@ extension RazeCore {
     public class Manager {
       
       // MARK: - Properties
-      private let session = URLSession.shared
+      internal var session: NetworkSession = URLSession.shared
       
       // MARK: - Object Lifecycle
       public init() {
@@ -29,13 +42,15 @@ extension RazeCore {
       }
       
       // MARK: - Body
+      /// Calls to the live internet to retrieve Data from specific location
+      /// - Parameters:
+      ///   - url: The location you wish to fetch data from
+      ///   - completionHandler: Returns a result object which signifies the status of the request
       public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
-        let task = session.dataTask(with: url) { data, response, error in
+        session.get(from: url) { data, error in
           let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
           completionHandler(result)
         }
-        
-        task.resume()
       }
     }
   }
--- Tests/RazeCoreTests/NetworkingTests.swift
+++ Tests/RazeCoreTests/NetworkingTests.swift
@@ -8,6 +8,17 @@
 import XCTest
 @testable import RazeCore
 
+public class NetworkSessionMock: NetworkSession {
+  
+  // MARK: - Properties
+  var data: Data?
+  var error: Error?
+  
+  public func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+    completionHandler(data, error)
+  }
+}
+
 class NetworkingTests: XCTestCase {
   
   static var allTests = [
@@ -16,16 +27,18 @@ class NetworkingTests: XCTestCase {
   
   func testLoadDataCall() {
     let manager = RazeCore.Networking.Manager()
+    let session = NetworkSessionMock()
+    manager.session = session
     let expectation = XCTestExpectation(description: "Call for data")
-    guard let url = URL(string: "http://raywenderlich.com") else {
-      return XCTFail("Coudl not create URL propertly")
-    }
+    let data = Data([0, 1, 0, 1])
+    session.data = data
+    let url = URL(fileURLWithPath: "url")
     
     manager.loadData(from: url) { result in
       expectation.fulfill()
       switch result {
       case .success(let returnedData):
-        XCTAssertNotNil(returnedData, "Response data is nil")
+        XCTAssertEqual(data, returnedData, "Manager returned unexpected data")
       case .failure(let error):
         XCTFail(error?.localizedDescription ?? "error forming error result")
       }
